b7949313e26898d597f54bc86614e60ebbdb4559
Nadikafiqri <nadikafiqri09@gmail.com>
2025-10-28 06:48:56 +0700

    tes2

---
+++ src/Donasi.java
@@ -0,0 +1,37 @@
+/**
+ * Kelas Donasi merepresentasikan data donasi yang diberikan oleh seorang donatur.
+ * Menyimpan nama donatur dan jumlah donasi.
+ */
+public class Donasi {
+    private String namaDonatur;
+    private double jumlahDonasi;
+
+    /**
+     * Konstruktor untuk membuat objek Donasi.
+     *
+     * @param namaDonatur nama dari donatur.
+     * @param jumlahDonasi jumlah donasi yang diberikan.
+     */
+    public Donasi(String namaDonatur, double jumlahDonasi) {
+        this.namaDonatur = namaDonatur;
+        this.jumlahDonasi = jumlahDonasi;
+    }
+
+    /**
+     * Mengambil nama donatur.
+     *
+     * @return nama donatur.
+     */
+    public String getNamaDonatur() {
+        return namaDonatur;
+    }
+
+    /**
+     * Mengambil jumlah donasi.
+     *
+     * @return jumlah donasi dalam bentuk double.
+     */
+    public double getJumlahDonasi() {
+        return jumlahDonasi;
+    }
+}
